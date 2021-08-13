2f5622cd697625e75e24203e174d2347b49f8a29
Tanzilal Mustaqim <derekln14@gmail.com>
2021-08-13 13:09:09 +0700

    add database

---
+++ aplikasi_b.sql
@@ -0,0 +1,47 @@
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (1, 'Dr. Christelle Swift', 'esporer@kreiger.biz', 65682663, NULL, '2021-08-10 10:09:14');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (2, 'Angie Lakin', 'jessie41@smitham.net', 89742416, NULL, '2021-08-10 10:09:14');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (3, 'Leon Waelchi', 'ustrosin@yahoo.com', 50793534, NULL, '2021-08-10 10:09:14');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (4, 'Elva Thompson', 'bettie.eichmann@hotmail.com', 96495060, NULL, '2021-08-10 10:09:14');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (5, 'Tia Schoen', 'hunter.luettgen@hotmail.com', 34577088, NULL, '2021-08-10 10:09:14');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (6, 'Margaret Langosh', 'kiehn.diana@hotmail.com', 56024038, NULL, '2021-08-10 10:09:14');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (7, 'Kirsten Pfeffer', 'rbauch@dicki.info', 60880708, NULL, '2021-08-10 10:09:15');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (8, 'Murphy Padberg', 'gnader@bergnaum.info', 5444757, NULL, '2021-08-10 10:09:15');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (9, 'Jonatan Larson', 'boris.wisozk@hotmail.com', 67062120, NULL, '2021-08-10 10:09:15');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (10, 'Mr. Elias Powlowski I', 'graham.michel@rice.com', 91232115, NULL, '2021-08-10 10:09:15');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (11, 'Jackeline Satterfield', 'collins.berta@gutkowski.net', 15636213, NULL, '2021-08-10 10:09:15');
+INSERT INTO "client" ("id", "name", "email", "retainer_fee", "updated_at", "created_at") VALUES (12, 'Anabel Hand', 'mckenzie19@yahoo.com', 19780889, NULL, '2021-08-10 10:09:15');
+INSERT INTO "migrations" ("id", "version", "class", "group", "namespace", "time", "batch") VALUES (1, '2021-08-03-051151', 'App\Database\Migrations\AddClient', 'default', 'App', 1628564896, 1);
+INSERT INTO "migrations" ("id", "version", "class", "group", "namespace", "time", "batch") VALUES (2, '2021-08-03-051805', 'App\Database\Migrations\AddUser', 'default', 'App', 1628564897, 1);
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (26, 'Jaylin Stamm', 'L', '5083 Koss Mall Suite 557
+Daughertychester, RI 02025-7264', '1-361-255-0544');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (27, 'Madyson Abbott', 'L', '8543 Heaven Port
+Creminmouth, NJ 13426', '424.754.5230');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (28, 'Alfredo Kirlin', 'L', '587 Hattie Plain
+Taliamouth, AR 93792', '(515) 806-6347');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (29, 'Mr. Grayson Connelly I', 'L', '89984 Schowalter Mount
+North Bernardo, CO 12221', '334-377-7211');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (30, 'Morton ehe', 'P', '70456 Champlin Ridges Port Isaac, ID 18358', '1-915-401-4726');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (31, 'Brennan Weber', 'L', '460 Isabelle Extension Apt. 020
+West Laceyside, AR 79956-4036', '1-820-862-9608');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (32, 'Noah Metz', 'L', '64499 Lea Courts
+Skilesville, NE 69611-8469', '1-210-880-0648');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (33, 'Alejandra Boehm', 'L', '847 Stanford Points
+West Zoeyton, MS 16391-5370', '(941) 313-3961');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (34, 'Felton Greenfelder Jr.', 'P', '26069 Alessandra Key
+Lake Dagmar, MO 10264-5999', '+19862403033');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (35, 'Abe Mueller IV', 'P', '85352 Wyatt Shores Apt. 710
+Schmelerport, NC 76495', '(925) 676-8395');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (36, 'Ned Sawayn DVM', 'L', '5649 Harvey Avenue
+Ladariusburgh, CO 26076', '207-447-0720');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (37, 'Reginald Keebler', 'L', '487 Gracie Plain
+Shainaport, NJ 39484-2060', '+1-217-522-1332');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (38, 'Marvel Pradipta', 'L', 'Jl Kamboja Kompl Amandole Bl I/19,Jati Pulo', '0215673207');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (39, 'Winda Nuraini', 'P', 'Gg. Orang No. 711, Batam 81180, Papua', '(+62) 522 3607 3370');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (40, 'cobaaa', 'P', 'jdskjjd', '0812232');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (41, 'cobaaa', 'P', 'jdskjjd', '0812232');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (43, 'Winda Nuraini', 'P', 'Gg. Orang No. 711, Batam 81180, Papua', '(+62) 522 3607 3370');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (44, 'Winda ajsdsajdshdjksd', 'P', 'Gg. Orang No. 711, Batam 81180, Papua', '(+62) 522 3607 3370');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (45, 'cobaaa', 'P', 'jdskjjd', '0812232');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (46, 'Joko Munandar', 'L', 'Jogja', '085364736476');
+INSERT INTO "patient" ("patient_id", "patient_name", "gender", "address", "telp") VALUES (47, 'cobaaa', 'P', 'jdskjjd', '0812232');
+INSERT INTO "user" ("id", "name", "email", "password", "updated_at", "created_at") VALUES (1, 'test', 'test@test.com', '$2y$10$klg0lS9gggQkXJHbFFLOj.fQOHAGx35/SV1DcFk5GSWzJMFIn31s.', NULL, '2021-08-10 10:20:34');
